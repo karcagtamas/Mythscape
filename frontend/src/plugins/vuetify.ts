0e81af5e4a32dd797dc3f0983d4d82f8a2c92d9b
Karcag Tamas <karcagtamas@outlook.com>
2025-02-23 10:38:27 +0100

    #14 Setup Theme and Typography

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -12,14 +12,13 @@ const myLightTheme: ThemeDefinition = {
   colors: {
     background: '#FFFFFF',
     surface: '#FFFFFF',
-    primary: '#6200EE',
-    'primary-darken-1': '#3700B3',
-    secondary: '#03DAC6',
-    'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
+    primary: '#6C0202',
+    secondary: '#02346C',
+    accent: '#556C02',
+    error: '#9F0000',
+    info: '#008C9F',
+    success: '#0E9F00',
+    warning: '#9F4A00',
   },
 }
 
