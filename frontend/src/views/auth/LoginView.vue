cd4473100893f5cf0b33f96f9a7db9a3794bbefd
Karcag Tamas <karcagtamas@outlook.com>
2025-02-23 10:23:32 +0100

    #4 Login page

--- frontend/src/views/auth/LoginView.vue
+++ frontend/src/views/auth/LoginView.vue
@@ -1,32 +1,96 @@
 <template>
   <v-card elevation="4" class="block">
-    <v-card-title class="text-h4 text-primary font-weight-bold">Login</v-card-title>
+    <v-card-title class="text-h5 text-primary font-weight-bold">Login</v-card-title>
     <v-card-text>
-      <v-form v-model="data.valid">
-        <v-text-field label="User Name" v-model="data.username"></v-text-field>
-        <v-text-field label="Password" v-model="data.password"></v-text-field>
-        <v-btn elevation="2" raised block color="primary" @click="submit" :disabled="!data.valid">Login</v-btn>
+      <v-form v-model="valid">
+        <v-text-field
+          label="User Name"
+          type="text"
+          density="compact"
+          variant="solo"
+          v-model="username"
+          prepend-inner-icon="mdi-account"
+          :error-messages="usernameErrors"
+          @blur="v$.username.$touch()"
+        ></v-text-field>
+        <v-text-field
+          label="Password"
+          :type="visisblePassword ? 'text' : 'password'"
+          density="compact"
+          variant="solo"
+          v-model="password"
+          :append-inner-icon="visisblePassword ? 'mdi-eye-off' : 'mdi-eye'"
+          prepend-inner-icon="mdi-lock-outline"
+          @click:append-inner="visisblePassword = !visisblePassword"
+          :error-messages="passwordErrors"
+          @blur="v$.password.$touch()"
+        ></v-text-field>
+        <v-btn elevation="2" raised block color="primary" @click="submit" :disabled="!valid"
+          >Login</v-btn
+        >
       </v-form>
     </v-card-text>
-    <v-card-actions>
-      <v-btn elevation="2" color="secondary">To Register</v-btn>
-    </v-card-actions>
+    <v-divider></v-divider>
+    <v-card-text>
+      <v-card variant="tonal">
+        <v-card-title></v-card-title>
+        <v-card-subtitle> If you does not have any account yet:</v-card-subtitle>
+        <v-card-actions class="justify-end">
+          <v-btn
+            variant="text"
+            color="secondary"
+            append-icon="mdi-chevron-right"
+            @click="$router.push({ path: '/auth/register' })"
+            >To Register</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-card-text>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+import { computed, ref } from 'vue'
 
-const data = ref({
-  username: '',
-  password: '',
-  valid: false,
-});
+const username = ref('')
+const password = ref('')
+const valid = ref(false)
+const visisblePassword = ref(false)
 
-function submit() {
-  // TODO:
+const rules = {
+  username: { required },
+  password: { required },
 }
 
+const v$ = useVuelidate(rules, { username, password })
+
+const usernameErrors = computed(() => {
+  if (!v$.value.username.$dirty) {
+    return []
+  }
+
+  return v$.value.username.required ? ['User Name is required'] : []
+})
+
+const passwordErrors = computed(() => {
+  if (!v$.value.password.$dirty) {
+    return []
+  }
+
+  return v$.value.password.required ? ['Password is required'] : []
+})
+
+function submit() {
+  v$.value.$touch()
+
+  if (v$.value.$invalid) {
+    return
+  }
+
+  // TODO: Submit
+}
 </script>
 
 <style lang="scss" scoped>
