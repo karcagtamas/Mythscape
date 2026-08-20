ece73be759c2141adcc0f192ab42f75d65b86ded
Karcag Tamas <karcagtamas@outlook.com>
2026-08-20 09:58:45 +0200

    #41 Refactor common server codes

--- backend/src/main/kotlin/utils/Dates.kt
+++ backend/src/main/kotlin/utils/Dates.kt
@@ -1,10 +1,10 @@
 package eu.karcags.mythscape.utils
 
-import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 
 fun current(): LocalDateTime {
     return Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
