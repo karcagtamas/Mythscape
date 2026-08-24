eb94233ab46f0a8e299ad64376f8d501c1cf83f3
Karcag Tamas <karcagtamas@outlook.com>
2026-08-24 12:38:38 +0200

    #42 Move server codes

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,19 +1,52 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 group = "eu.karcags.mythscape"
 version = "1.0.0"
+
 application {
     mainClass = "eu.karcags.mythscape.ApplicationKt"
+
+    applicationDefaultJvmArgs = listOf("-Dfile.encoding=UTF-8")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 dependencies {
     api(project(":core"))
-    implementation(libs.logback)
+
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.status.pages)
+    implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.host)
+    implementation(libs.ktor.server.callLogging)
+    implementation(libs.ktor.server.swagger)
+    implementation(libs.ktor.server.openapi)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.ktor.server.request.validation)
+    implementation(libs.ktor.server.cors)
+    implementation(libs.ktor.certificates)
+
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.kotlin.datetime)
+
+    implementation(libs.postgresql)
+    implementation(libs.logback)
+    implementation(libs.jbcrypt)
+
+    implementation(libs.koin.ktor)
+    implementation(libs.koin.logger)
+
     testImplementation(libs.ktor.server.testHost)
     testImplementation(libs.kotlin.testJunit)
 }
