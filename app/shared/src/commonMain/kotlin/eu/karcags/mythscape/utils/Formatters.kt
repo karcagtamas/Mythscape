cedb931e8c4520070594804763d4af9a8f293d1b
Karcag Tamas <karcagtamas@outlook.com>
2026-09-02 15:32:55 +0200

    #42 Add local date time formatter

--- app/shared/src/commonMain/kotlin/eu/karcags/mythscape/utils/Formatters.kt
+++ app/shared/src/commonMain/kotlin/eu/karcags/mythscape/utils/Formatters.kt
@@ -1,7 +1,22 @@
 package eu.karcags.mythscape.utils
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.format
+
+val localDateTimeFormat = LocalDateTime.Format {
+    year();
+    chars("-")
+    monthNumber()
+    chars("-")
+    day()
+    chars(" ")
+    hour()
+    chars(":")
+    minute()
+    chars(":")
+    second()
+}
 
 fun LocalDateTime.formatted(): String {
-    return toString()
+    return format(localDateTimeFormat)
 }
